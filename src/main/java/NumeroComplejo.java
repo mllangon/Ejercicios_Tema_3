ae1f394e9e81f9e1ba37c1b2597e314d6944226f
mllangon <mllangon@myuax.com>
2024-05-03 13:07:02 +0200

    9

--- src/main/java/NumeroComplejo.java
+++ src/main/java/NumeroComplejo.java
@@ -6,18 +6,20 @@ public class NumeroComplejo {
     private double parteReal;
     private double parteImaginaria;
 
-    // Constructor por defecto que inicializa un número complejo en 0 + 0i
     public NumeroComplejo() {
         this(0, 0);
     }
 
-    // Constructor que acepta parte real e imaginaria
     public NumeroComplejo(double parteReal, double parteImaginaria) {
         this.parteReal = parteReal;
         this.parteImaginaria = parteImaginaria;
     }
 
-    // Método para sumar dos números complejos
+    public NumeroComplejo(NumeroComplejo otro) {
+        this.parteReal = otro.parteReal;
+        this.parteImaginaria = otro.parteImaginaria;
+    }
+
     public NumeroComplejo sumar(NumeroComplejo otro) {
         return new NumeroComplejo(
                 this.parteReal + otro.parteReal,
@@ -25,10 +27,22 @@ public class NumeroComplejo {
         );
     }
 
-    // Método para multiplicar dos números complejos
+    public NumeroComplejo restar(NumeroComplejo otro) {
+        return new NumeroComplejo(
+                this.parteReal - otro.parteReal,
+                this.parteImaginaria - otro.parteImaginaria
+        );
+    }
+
     public NumeroComplejo multiplicar(NumeroComplejo otro) {
         double nuevaParteReal = this.parteReal * otro.parteReal - this.parteImaginaria * otro.parteImaginaria;
         double nuevaParteImaginaria = this.parteReal * otro.parteImaginaria + this.parteImaginaria * otro.parteReal;
+        return new NumeroComplejo(nuevaParteReal, nuevaParteImaginaria);
+    }
+
+    public NumeroComplejo dividir(NumeroComplejo otro) {
+        double nuevaParteReal = (this.parteReal * otro.parteReal + this.parteImaginaria * otro.parteImaginaria) / (Math.pow(otro.parteReal, 2) + Math.pow(otro.parteImaginaria, 2));
+        double nuevaParteImaginaria = (this.parteImaginaria * otro.parteReal - this.parteReal * otro.parteImaginaria) / (Math.pow(otro.parteReal, 2) + Math.pow(otro.parteImaginaria, 2));
         return new NumeroComplejo(nuevaParteReal, nuevaParteImaginaria);
     }
 
@@ -41,33 +55,78 @@ public class NumeroComplejo {
         JFrame frame = new JFrame("Calculadora de Números Complejos");
         frame.setLayout(new FlowLayout());
 
-        NumeroComplejo c1 = new NumeroComplejo(3, 2);
-        NumeroComplejo c2 = new NumeroComplejo(1, 7);
-
+        JTextField txtComplejo1 = new JTextField(10);
+        JTextField txtComplejo2 = new JTextField(10);
         JTextArea textArea = new JTextArea(10, 20);
         textArea.setEditable(false);
-        JButton btnSuma = new JButton("Sumar (3+2i) + (1+7i)");
-        JButton btnMultiplicacion = new JButton("Multiplicar (3+2i) * (1+7i)");
+        JButton btnSuma = new JButton("Sumar");
+        JButton btnResta = new JButton("Restar");
+        JButton btnMultiplicacion = new JButton("Multiplicar");
+        JButton btnDivision = new JButton("Dividir");
 
         btnSuma.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                NumeroComplejo c1 = parseComplejo(txtComplejo1.getText());
+                NumeroComplejo c2 = parseComplejo(txtComplejo2.getText());
                 NumeroComplejo resultado = c1.sumar(c2);
                 textArea.setText("Suma: " + resultado.toString());
             }
         });
 
+        btnResta.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                NumeroComplejo c1 = parseComplejo(txtComplejo1.getText());
+                NumeroComplejo c2 = parseComplejo(txtComplejo2.getText());
+                NumeroComplejo resultado = c1.restar(c2);
+                textArea.setText("Resta: " + resultado.toString());
+            }
+        });
+
         btnMultiplicacion.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                NumeroComplejo c1 = parseComplejo(txtComplejo1.getText());
+                NumeroComplejo c2 = parseComplejo(txtComplejo2.getText());
                 NumeroComplejo resultado = c1.multiplicar(c2);
                 textArea.setText("Multiplicación: " + resultado.toString());
             }
         });
 
+        btnDivision.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                NumeroComplejo c1 = parseComplejo(txtComplejo1.getText());
+                NumeroComplejo c2 = parseComplejo(txtComplejo2.getText());
+                NumeroComplejo resultado = c1.dividir(c2);
+                textArea.setText("División: " + resultado.toString());
+            }
+        });
+
+        frame.getContentPane().setBackground(new Color(173, 216, 230)); // RGB para azul claro
+        txtComplejo1.setBackground(new Color(173, 216, 230));
+        txtComplejo2.setBackground(new Color(173, 216, 230));
+        btnSuma.setBackground(new Color(173, 216, 230));
+        btnResta.setBackground(new Color(173, 216, 230));
+        btnMultiplicacion.setBackground(new Color(173, 216, 230));
+        btnDivision.setBackground(new Color(173, 216, 230));
+        textArea.setBackground(new Color(173, 216, 230));
+
+        frame.add(new JLabel("Complejo 1 (x+yi):"));
+        frame.add(txtComplejo1);
+        frame.add(new JLabel("Complejo 2 (x+yi):"));
+        frame.add(txtComplejo2);
         frame.add(btnSuma);
+        frame.add(btnResta);
         frame.add(btnMultiplicacion);
+        frame.add(btnDivision);
         frame.add(new JScrollPane(textArea));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private static NumeroComplejo parseComplejo(String s) {
+        String[] parts = s.split("\\+");
+        double parteReal = Double.parseDouble(parts[0]);
+        double parteImaginaria = Double.parseDouble(parts[1].replace("i", ""));
+        return new NumeroComplejo(parteReal, parteImaginaria);
     }
 }
